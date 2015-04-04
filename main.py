e1bcbf5567be48b2f1d4db327b5249be590b7de8
hasitha.prageeth <haztha@gmail.com>
2015-04-04 18:43:10 +0530

    Redirects and Escape Html Correction
    escape_html(s) function
    cgi.escape(s, quote=True)
    
    redirect("/thanks")
    class ThanksHandler(webapp2.RequestHandler)

--- main.py
+++ main.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 # 
 import webapp2
+import cgi
 
 form="""
     <!DOCTYPE HTML>
@@ -36,6 +37,18 @@ form="""
     </body>
     </html>
 """
+#success="""
+    #<!DOCTYPE HTML>
+    #<html>
+    #    <head>
+    #    <title>My Site</title>
+    #</head>
+    #<body>
+    #    <h1>Thanks! That's totally a valid date!</h1>
+    #</body>
+    #</html>   
+#"""
+
 months=['January',
         'February',
         'March',
@@ -67,12 +80,17 @@ def valid_year(year):
         year = int(year)
         if 1900<year<=2020:
             return year
-        
 
-        
+def escape_html(s):
+    return cgi.escape(s, quote=True)
+
+    #for (i,o) in (("&","&amp;"),(">","&gt;"),("<","&lt;"),(""","&quot;")):
+        #s = s.replace(i,o)
+    #return s
+
 class MainHandler(webapp2.RequestHandler):
     def write_form(self, error="", month="", day="", year=""):
-        self.response.out.write(form % {"error": error, "month": month, "day": day, "year": year})
+        self.response.out.write(form % {"error": error, "month": escape_html(month), "day": escape_html(day), "year": escape_html(year)})
         
     def get(self):
         self.write_form()
@@ -90,7 +108,11 @@ class MainHandler(webapp2.RequestHandler):
             self.write_form("That doesen't look right", user_month, user_day, user_year)
 
         else:            
-            self.response.out.write("Thanks! That's totally a valid day!")
+            self.redirect("/thanks")
 
-app = webapp2.WSGIApplication([('/', MainHandler)],
+class ThanksHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write("Thanks! That's totally a valid date!")
+
+app = webapp2.WSGIApplication([('/', MainHandler),('/thanks', ThanksHandler)],
                               debug=True)
