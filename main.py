1586c03abc3be5dcaf5f24893e9c2ad753c04f78
hasitha.prageeth <haztha@gmail.com>
2015-04-04 10:34:26 +0530

    type="checkbox"
    ?q=on   ---- for on
    ?            ---- for off

--- main.py
+++ main.py
@@ -13,14 +13,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+# 
 import webapp2
 
 form="""
     <!DOCTYPE HTML>
     <html>
     <form method="get" action="/testform">
-      <input type="password" name="q"></input>
+      <input type="checkbox" name="q"></input>
       <input type="submit"></input>
     </form>
     </html>
