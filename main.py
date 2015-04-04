fb43eb1be888a976669002d61a3f4bb1457c719b
hasitha.prageeth <haztha@gmail.com>
2015-04-04 15:11:23 +0530

    Form Validation
    Data Validation (Birthday)
    valid_month(month)
    valid_day(month)
    valid_year(year)
    
    -- dictionary to map data
    -- post method to change data

--- main.py
+++ main.py
@@ -7,7 +7,6 @@
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
-#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,30 +18,66 @@ import webapp2
 form="""
     <!DOCTYPE HTML>
     <html>
-    <form method="get" action="/testform">
-      <select name="q">
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-      </select>
+    <form method="post">
+      What is your birthday?
+      <br>
+      <label>Month <input type="text" name="month"></input></label>
+      <label>Day <input type="text" name="day"></input></label>
+      <label>Year <input type="text" name="year"></input</label>
+      <br>
       <br>
       <input type="submit"></input>
     </form>
     </html>
 """
+months=['January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December']
 
+month_abbvs =dict((m[:3].lower(),m) for m in months)
+
+def valid_month(month):
+    if month:
+        short_month = month[:3].lower()
+        return month_abbvs.get(short_month)
+    
+def valid_day(day):
+    if day and day.isdigit():
+        day = int(day)
+        if 0<day<32:
+            return day
+
+def valid_year(year):
+    if year and year.isdigit():
+        year = int(year)
+        if 1900<year<=2020:
+            return year
+        
+
+        
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        #self.response.headers['Content-Type']='text/html'
         self.response.out.write(form)
-        
-class TestHandler(webapp2.RequestHandler):
-    def get(self):
-        q=self.request.get("q")
-        self.response.out.write(q)
-        #self.response.headers['Content-Type']='text/plain'
-        #self.response.out.write(self.request)
 
+    def post(self):
+        user_month = valid_month(self.request.get('month'))
+        user_day = valid_day(self.request.get('day'))
+        user_year = valid_year(self.request.get('year'))
 
-app = webapp2.WSGIApplication([('/', MainHandler),('/testform', TestHandler)],
+        if not (user_month and user_day and user_year):
+            self.response.out.write(form)
+
+        else:            
+            self.response.out.write("Thanks! That's totally a valid day!")
+
+app = webapp2.WSGIApplication([('/', MainHandler)],
                               debug=True)
